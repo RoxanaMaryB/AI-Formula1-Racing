e4a21ff89d1f264dcb41ec0b99e0cc1b72ac9ad8
pofian <andrei.filip.popa@gmail.com>
2024-12-18 15:42:18 +0200

    Added rotating to car

--- car.py
+++ car.py
@@ -4,19 +4,41 @@ import pygame
 class Car:
 
     def __init__(self, sprite, size, position):
+        self.must_draw = False
+
         self.sprite = sprite
         self.position = position
+        self.set_angle(0)
         self.set_size(size)
 
         self.speed = 0
         self.distance = 0
         self.time = 0
         self.alive = True
-    
+
+    def start_drawing(self):
+        if self.must_draw:
+            return
+        self.must_draw = True
+        self.sprite_resized = pygame.transform.scale(self.sprite, self.size)
+        self.rotate_sprite()
+
+    def stop_drawing(self):
+        self.must_draw = False
+
     def set_size(self, size):
         self.size = size
-        self.sprite_resized = pygame.transform.scale(self.sprite, (size[0], size[1]))
-        pass
+        self.sprite_resized = pygame.transform.scale(self.sprite, self.size)
+        if self.must_draw:
+            self.rotate_sprite()
+
+    def set_angle(self, angle):
+        self.angle = angle
+        self.rotate_sprite()
+
+    def rotate_sprite(self):
+        if self.must_draw:
+            self.rotated_sprite = pygame.transform.rotate(self.sprite_resized, self.angle)
 
     def set_position(self, position):
         self.position = position
@@ -25,7 +47,8 @@ class Car:
         return self.alive
 
     def draw(self, screen):
-        screen.blit(self.sprite_resized, self.position)
+        if self.must_draw:
+            screen.blit(self.rotated_sprite, self.position)
 
     def check_collision(self, map):
         pass
--- position_calculator.py
+++ position_calculator.py
@@ -9,12 +9,39 @@ def loop(game):
     car_size = [100, 50]
     car_position = [1220, 820]
     car = Car(game.car_sprite, car_size, car_position)
+    car.start_drawing()
     game.add_car(car)
     with open(log_file, "w") as file:
         file.write(f"map: {map_file}\n")
 
     while True:
         game.update_screen()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            car_position[0] -= car_size[0] / 200
+            car.set_position(car_position)
+        if keys[pygame.K_d]:
+            car_position[0] += car_size[0] / 200
+            car.set_position(car_position)
+        if keys[pygame.K_w]:
+            car_position[1] -= car_size[1] / 200
+            car.set_position(car_position)
+        if keys[pygame.K_s]:
+            car_position[1] += car_size[1] / 200
+            car.set_position(car_position)
+
+        if keys[pygame.K_z]:
+            car_size = [size / 1.01 for size in car_size]
+            car.set_size(car_size)
+        if keys[pygame.K_x]:
+            car_size = [size * 1.01 for size in car_size]
+            car.set_size(car_size)
+
+        if keys[pygame.K_q]:
+            car.set_angle(car.angle + 0.5)
+        if keys[pygame.K_e]:
+            car.set_angle(car.angle - 0.5)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -22,26 +49,6 @@ def loop(game):
                 match event.key:
                     case pygame.K_ESCAPE:
                         pygame.quit()
-
-                    case pygame.K_z:
-                        car_size = [size * 0.8 for size in car_size]
-                        car.set_size(car_size)
-                    case pygame.K_x:
-                        car_size = [size * 1.25 for size in car_size]
-                        car.set_size(car_size)
-
-                    case pygame.K_a:
-                        car_position[0] -= car_size[0] / 4
-                        car.set_position(car_position)
-                    case pygame.K_d:
-                        car_position[0] += car_size[0] / 4
-                        car.set_position(car_position)
-                    case pygame.K_w:
-                        car_position[1] -= car_size[1] / 4
-                        car.set_position(car_position)
-                    case pygame.K_s:
-                        car_position[1] += car_size[1] / 4
-                        car.set_position(car_position)
                     case pygame.K_l:
                         with open("car_position.txt", "a") as file:
                             file.write(f"size: {car_size}, position: {car_position}\n")
