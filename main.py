15538aafa2e6043b561ad8c93d3e4be0934ff8e5
Militaru Ionut-Marius <militarumarius107@gmail.com>
2025-01-04 18:59:37 +0200

    train the cars, on every map, to get the best car for at least 100 generation each, save the populantion

--- main.py
+++ main.py
@@ -1,65 +1,67 @@
 from model.ai_car import *
-from usercar import *
+from model.game import *
+from car import *
 import pickle
 import neat
 import pygame
 
-# clasa Car
+map_name  = "finish_line"
+
+directory = "maps/" + map_name + "/"
+map_file      = directory + "map.png"
+best_car_file = directory + 'best_car_100.pkl'
+
+ai_car_file   = "cars/blue_car.png"
+user_car_file = "cars/red_car.png"
+
+def load_best_car(file_name):
+    try:
+        with open(file_name, "rb") as f:
+            best_car , config= pickle.load(f)
+            return best_car, config
+    except FileNotFoundError:
+        print(f"File {file_name} not found.")
+        return None, None
+
+
+def init_game():
+    global game
+    game = Game([1000, 500], map_file, ai_car_file)
+    user_car_sprite = pygame.image.load(user_car_file).convert()
+    user_car = Car(game, True, user_car_sprite, size = [100, 50], start_position = [1220, 820])
+    game.add_user_car(user_car)
 
 def main():
     dimensions = [900, 500]
-    map_file = "maps/finish_line.png"
-    car_file = "maps/blue_car.png"
     screen = pygame.display.set_mode(dimensions, pygame.RESIZABLE)
-    game = Game(dimensions, map_file, car_file)
+    game = Game(dimensions, map_file, ai_car_file)
     font = pygame.font.SysFont(None, 55)
-    with open('winner.pkl', "rb") as f:
-        best_genome, config = pickle.load(f)
-    net = neat.nn.FeedForwardNetwork.create(best_genome, config)
+    best_car, config = load_best_car(best_car_file)
+    if best_car is not None:
+        print(f"Loaded best car from {best_car_file}")
+    else:
+        print(f"Failed to load best car from {best_car_file}")
+    net = neat.nn.FeedForwardNetwork.create(best_car, config)
     # initial_position = ([1220, 820], [100, 50])
-    player_car = UserCar(car_file)
-    ai_car = AICar(game, must_draw=True, sprite=pygame.image.load(car_file))
-
-    game.add_user_car(player_car)
+    player_car = Car(ai_car_file)
+    ai_car = AICar(game, must_draw=True, sprite=pygame.image.load(ai_car_file))
     game.add_ai_car(ai_car)
 
-    running = True
-    while running:
-        player_input = {"steering": 0, "acceleration": 0}
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player_input["steering"] = -1
-                elif event.key == pygame.K_RIGHT:
-                    player_input["steering"] = 1
-                elif event.key == pygame.K_UP:
-                    player_input["acceleration"] = 1
-                elif event.key == pygame.K_DOWN:
-                    player_input["acceleration"] = -1
+   
+    if game.pixel_out_of_bounds(player_car.position):
+        print("Player car out of bounds")
+        winner = "AI Car Wins!"
+        running = False
+    elif game.pixel_out_of_bounds(ai_car.position):
+        print("AI car out of bounds")
+        winner = "User Car Wins!"
+        running = False
 
-        ai_inputs = ai_car.get_data()
-        actions = net.activate(ai_inputs)
-        choice = actions.index(max(actions))
+    screen.fill((0, 0, 0))
+    game.update()
+    pygame.display.update()
 
-        player_car.update(player_input)
-        ai_car.update_position(choice)
-
-        if game.pixel_out_of_bounds(player_car.position):
-            print("Player car out of bounds")
-            winner = "AI Car Wins!"
-            running = False
-        elif game.pixel_out_of_bounds(ai_car.position):
-            print("AI car out of bounds")
-            winner = "User Car Wins!"
-            running = False
-
-        screen.fill((0, 0, 0))
-        game.update()
-        pygame.display.update()
-
-        pygame.time.delay(16)
+    pygame.time.delay(16)
 
     if winner:
         screen.fill((0, 0, 0))
